520f882b9627529cac897fbdb61cc7ca3c88322f
jims55556 <94209037@qq.com>
2021-03-05 16:08:23 +0800

    Update TestRequest.py

--- TestRequest.py
+++ TestRequest.py
@@ -85,7 +85,34 @@ def TestGetRequest(hurl,hdata,headers,htestcassid,htestcasename,htesthope,fanhui
         logger.error("实际返回结果：" + str(hresult))
     print(hlist)
 
-
+def TestDeleteRequest(hurl,hdata,headers,htestcassid,htestcasename,htesthope,fanhuitesthope):
+    hr = requests.delete(hurl, data=hdata, headers=headers)#这个是删除请求使用的方法
+    # hr=requests.post(hurl,data=json.dumps(hdata),headers=header)#同post请求的解释相一致
+    hresult=json.loads(hr.text)
+    hstatus=hresult['status']
+    if hstatus==htesthope and fanhuitesthope in str(hresult):
+        hhhdata={"t_id":htestcassid,
+                 "t_name":htestcasename,
+                 "t_method":"POST",
+                "t_url":hurl,
+                 "t_param":"测试数据:"+str(hdata),
+                 "t_hope":"status:"+htesthope+"期望结果："+fanhuitesthope,
+                 "t_actual":"status:"+hstatus+"实际返回结果："+str(hresult),
+                 "t_result":"通过"
+        }
+        hlist.append(hhhdata)
+    else:
+        hhhdata = {"t_id": htestcassid,
+                   "t_name": htestcasename,
+                   "t_method": "POST",
+                   "t_url": hurl,
+                   "t_param": "测试数据:" + str(hdata),
+                   "t_hope": "status:" + htesthope + "期望结果：" + fanhuitesthope,
+                   "t_actual": "status:" + hstatus + "实际返回结果：" + str(hresult),
+                   "t_result": "失败"
+                   }
+        hlist.append(hhhdata)
+    print(hlist)
 
 
 
